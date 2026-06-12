0d79a44f7a8f8a2996ed338fb8b78fb3a82fb103
Dimas Erlangga Putera <rvpinoz@gmail.com>
2026-06-12 11:18:38 +0700

    add link support me on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,11 +23,10 @@ export const Footer = () => {
       >
         <Text variant="body-default-s" onBackground="neutral-strong">
           <Text onBackground="neutral-weak">© {currentYear} /</Text>
-          <Text paddingX="4">{person.name}</Text>
+          <Text paddingX="4">{person.name} |</Text>
+          <Text>{person.role} </Text>
           <Text onBackground="neutral-weak">
-            {/* Usage of this template requires attribution. Please don't remove the link to Once UI unless you have a Pro license. */}
-            |{" "}
-            <SmartLink href="#">Home Brewer</SmartLink>
+            / <SmartLink href="https://saweria.co/derd">Buy Me a Coffee ☕</SmartLink>
           </Text>
         </Text>
         <Row gap="16">
